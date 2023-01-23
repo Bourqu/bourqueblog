58eea604d3f8f484df42420678596f1b609eda28
Bourqu <dybourqu@gmail.com>
2023-01-23 16:42:04 -0500

    create project card component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,16 @@
 <script>
     export let data; /*initializes the props*/
+    import ProjectCard from '$lib/components/project-card.svelte'
   </script>
   
-  <!--<pre>{JSON.stringify(data, null, 2)}</pre>-->
-  <p> {data.projects[0].name} </p>
+  
+
 
   <h1>Recent Projects by Me</h1>
 
 <div>
   {#each data.projects as { name, slug, description, image }}
-    <div>
-      <img src={image[0].url} alt={name} />
-      <a href={`/projects/${slug}`}>
-        <div>
-          <h2>{name}</h2>
-          <p>
-            {description.slice(0, 80)}...
-          </p>
-        </div>
-      </a>
-    </div>
+  <ProjectCard {name} {description} url={image[0].url} {slug} />
+
   {/each}
 </div>
