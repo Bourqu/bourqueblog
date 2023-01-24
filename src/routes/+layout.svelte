3142d1ccf6290dfe5be7c31bc126ae93418ab59e
Bourqu <dybourqu@gmail.com>
2023-01-23 19:14:38 -0500

    added tailwaind and daisyui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,22 @@
 <script>
-    import Header from './Header.svelte';
-    import Footer from './Footer.svelte';
+	import '../app.postcss';
+	import Header from './Header.svelte';
+	import Footer from './Footer.svelte';
 </script>
 
-<Header/>
+<header />
 
 <main>
-    <slot/> 
-<!-- sveltkit is a decorate filed format, recall + -->
-<!-- +layout is another type-->
-<!-- in svelte we have concept of slot or child. we use <slot/> to show the insertion of children
+	<slot />
+	<!-- sveltkit is a decorate filed format, recall + -->
+	<!-- +layout is another type-->
+	<!-- in svelte we have concept of slot or child. we use <slot/> to show the insertion of children
 when we change pages on the slot content will be updated -->
 </main>
 
-<Footer/>
+<footer />
 
 <!--as the parent directory routes, the header and footer will be in all. this work for subdirs too
 by creating layout groups we can eventually add mroe control to this
 
 -->
-
-
-
-
-
-
-
