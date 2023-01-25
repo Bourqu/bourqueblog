c44a00aa1becf54457758b6c9e2aaffc938f473f
Bourqu <dybourqu@gmail.com>
2023-01-24 19:35:38 -0500

    added default sitemap

--- src/lib/graphql-queries.js
+++ src/lib/graphql-queries.js
@@ -1,20 +1,5 @@
 import { gql } from 'graphql-request';
 
-export const projectsQuery = gql`
-	query GetProjects {
-		projects {
-			name
-			slug
-			description
-			demo
-			sourceCode
-			image {
-				url
-			}
-		}
-	}
-`;
-
 export const authorsQuery = gql`
 	query GetAuthors {
 		authors {
@@ -25,6 +10,38 @@ export const authorsQuery = gql`
 			picture {
 				url
 			}
+		}
+	}
+`;
+
+const PROJECT_FRAGMENT = gql`
+	fragment ProjectDetails on Project {
+		name
+		slug
+		description
+		tags
+		demo
+		sourceCode
+		image {
+			url
+		}
+	}
+`;
+
+export const projectsQuery = gql`
+	${PROJECT_FRAGMENT}
+	query GetProjects {
+		projects {
+			...ProjectDetails
+		}
+	}
+`;
+
+export const projectQuery = gql`
+	${PROJECT_FRAGMENT}
+	query GetProject($slug: String!) {
+		project(where: { slug: $slug }) {
+			...ProjectDetails
 		}
 	}
 `;
